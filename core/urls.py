67384493fb1f8ed3a30abcf5f45db91f19ec2a53
coder-ph <phelixmbani@gmail.com>
2025-07-07 21:26:14 +0300

    simplejwt

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,16 @@
 """core/urls.py."""
 
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import GISLayerViewSet, SiteViewSet
+
+router = DefaultRouter()
+router.register(r"sites", SiteViewSet, basename="site")
+router.register(r"gis-layers", GISLayerViewSet, basename="gis-layer")
+
 
 urlpatterns = [
+    path("", include(router.urls)),  # Include the router's URLs
     path("example/", lambda request: None, name="example"),  # Example endpoint
 ]
