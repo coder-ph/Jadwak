ffd0d45704a1c5670de7ad5b2d4e361fe09d60ad
coder-ph <phelixmbani@gmail.com>
2025-07-08 11:59:40 +0300

    file upload config

--- Jadwak/urls.py
+++ Jadwak/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
@@ -56,3 +58,7 @@ urlpatterns = [
     ),
     # path("api/v1/auth/", include("rest_framework_simplejwt.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
